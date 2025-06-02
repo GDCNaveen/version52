658920396324ec9851a755a9b3aaba3b1e21e435
GDCNaveen <algotnaveen@gmail.com>
2025-06-02 15:23:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,10 @@ const videos = [
   },
   {
     url: "https://player.vimeo.com/video/1089625015?h=f0067b5f9b&autoplay=1&muted=1"
-  }
+  },
+  {
+    url: "https://player.vimeo.com/video/1089631527?h=d67902f963&autoplay=1&muted=1"
+  },
 ];
 
 const videoList = document.getElementById("videoList");
